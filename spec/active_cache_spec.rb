9beed5ecc579939a196ff09bef4c5ae305d79ed1
yu-croco <yuki.kita22@gmail.com>
2019-05-04 19:14:47 +0900

    environment setup

--- spec/active_cache_spec.rb
+++ spec/active_cache_spec.rb
@@ -1,9 +1,5 @@
 RSpec.describe ActiveCache do
-  it "has a version number" do
-    expect(ActiveCache::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'should be true' do
+    expect(true).to eq true
   end
 end
