9731ff94f37b59a41854a0a18f2ce71ed42fe243
yu-croco <yuki.kita22@gmail.com>
2019-05-04 19:04:29 +0900

    initial commit

---
+++ lib/active_cache.rb
@@ -0,0 +1,6 @@
+require "active_cache/version"
+
+module ActiveCache
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/active_cache/version.rb
@@ -0,0 +1,3 @@
+module ActiveCache
+  VERSION = "0.1.0"
+end
