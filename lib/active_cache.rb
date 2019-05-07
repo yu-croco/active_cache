248cd8ce61d41d330d26716d7b126e276164e1ad
yu-croco <yuki.kita22@gmail.com>
2019-05-07 11:15:04 +0900

    let include lazy

--- lib/active_cache.rb
+++ lib/active_cache.rb
@@ -1,3 +1,4 @@
+require 'active_support/lazy_load_hooks'
 require 'active_record'
 require 'active_cache/version'
 require 'active_cache/config'
@@ -5,6 +6,6 @@ require 'active_cache/cache_key_format'
 require 'active_cache/cache'
 require 'active_cache/find'
 
-class ActiveRecord::Base
-  include ActiveCache::Find
+ActiveSupport.on_load :active_record do
+  ::ActiveRecord::Base.send :include, ActiveCache::Find
 end
