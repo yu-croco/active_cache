c395efbfda2d88337bdde43f2e4c3ac550dbec99
yu-croco <yuki.kita22@gmail.com>
2019-08-03 20:06:01 +0900

    change: validate against array argument

--- lib/active_cache/find_with_cache.rb
+++ lib/active_cache/find_with_cache.rb
@@ -7,13 +7,14 @@ module ActiveCache
         @is_cache_find ||= flg
       end
 
-      def find_with_cache(*args)
-        if find_with_cache?
-          ActiveCache::Loggable.stored(args.first)
+      def find_with_cache(id)
+        raise ArgumentError, 'Invalid Argument(id)' unless id.is_a?(Integer)
 
-          cache_key = CacheKeyFormat.create_key(args.first)
+        if find_with_cache?
+          ActiveCache::Loggable.stored(id)
+          cache_key = CacheKeyFormat.create_key(id)
           ActiveCache::Cache.fetch(cache_key) do
-            find(*args)
+            find(id)
           end
         end
       end
--- spec/active_cache/find_with_cache_spec.rb
+++ spec/active_cache/find_with_cache_spec.rb
@@ -14,11 +14,8 @@ RSpec.describe ActiveCache do
 
     context 'if args is Array' do
       let!(:user2){User.create(first_name: "Taro", last_name: "Yamada", age: 35)}
-      it 'data should not be stored in cache after find' do
-        expect(ActiveCache::Cache.exist?("active_cache/user/find/#{user.id}")).to eq false
-        User.find_with_cache([user.id, user2.id])
-        expect(ActiveCache::Cache.exist?("active_cache/user/find/#{user.id}")).to eq false
-        expect(ActiveCache::Cache.exist?("active_cache/user/find/#{user2.id}")).to eq false
+      it 'raise ArgumentError' do
+        expect { User.find_with_cache([user.id, user2.id]) }.to raise_error(ArgumentError)
       end
     end
   end
